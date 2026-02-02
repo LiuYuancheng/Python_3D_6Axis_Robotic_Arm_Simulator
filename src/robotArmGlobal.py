ce5da34341950478b9f862a168abd01a938b55dd
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2026-02-02 16:03:18 +0800

    Added the arm simulator main module.

---
+++ src/robotArmGlobal.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------
+# Name:        robotArmGlobal.py
+#
+# Purpose:     This module is a simple simulator for a 3D 6Axis robotic arm by 
+#              using the wxPython and OpenGL library.
+#
+# Author:      Yuancheng Liu
+#
+# Created:     2026/01/20
+# Version:     v_0.0.1
+# Copyright:   Copyright (c) 2026 LiuYuancheng
+# License:     MIT License
+#-----------------------------------------------------------------------------
